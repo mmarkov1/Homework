7d06837e731e5edecb716d559a720061771e4cc0
Максим Марков <legkiyprocces@mail.ru>
2021-11-09 17:33:57 +0500

    updated prog

--- lesson3/2/ConsoleApp1/ConsoleApp1/Program.cs
+++ lesson3/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,14 +11,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string[,] numbook = new string[5, 2]; // Создается массив, 5 строк, 2 слова
-            for (int i = 0; i < numbook.GetLength(0); i++)
+            for (int i = 0; i < numbook.GetLength(0); i++) //Цикл для ввода имени и номера
             {
                 Console.WriteLine("Введите имя " + (i + 1) + " контакта");
                 numbook[i, 0] = Console.ReadLine();
                 Console.WriteLine("Введите номер или емейл " + (i + 1) + " контакта");
                 numbook[i, 1] = Console.ReadLine();
             }
-            for (int i = 0; i < numbook.GetLength(0); i++)
+            for (int i = 0; i < numbook.GetLength(0); i++) // Показ того, что ввели
             {
                 Console.Write("Контакт " + (i + 1) + ": " + numbook[i, 0] + " : " + numbook[i, 1]);
                 Console.WriteLine();
