00dfd3ac6190f4a0f4bb2323f02e53c920c90ece
Максим Марков <legkiyprocces@mail.ru>
2021-11-09 18:16:35 +0500

    added prog 3

---
+++ lesson3/3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите слово");
+        char[] charmas = Console.ReadLine().ToCharArray();
+        for (int i = 0; i < charmas.Length; i++) 
+        {
+            Console.Write(charmas[charmas.Length - i - 1]);
+        }
+    }
+}
