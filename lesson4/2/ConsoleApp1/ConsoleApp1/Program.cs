77e6659c75128cb40f0d57ad743137b410fa402a
Максим Марков <legkiyprocces@mail.ru>
2021-11-12 17:51:48 +0500

    Добавлено 2 задание

---
+++ lesson4/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите числа через пробел: ");
+            string[] massive = (Console.ReadLine().Split(' ')); // Пользователь вводит только числа, но используем строку, чтобы потом в методе ее преобразовать в число
+            Console.WriteLine(PlusAll(massive));
+            Console.ReadLine();
+        }
+
+        static string PlusAll(string[] massive) 
+        {
+            int sum = 0;
+            int check = 0; //Нужно для проверки числа на знаки
+            for (int i = 0; i < massive.Length; i++) 
+            {
+                int number = 0;
+                bool canConvert = int.TryParse(massive[i], out number); // Проверка на то, что все знаки являются числами
+                if (canConvert == true) // Если проверка удалась, цикл идет дальше
+                {
+                    int num = Int32.Parse(massive[i]); // По условию сказано, что пользователь вводит набор чисел
+                    sum += num;
+                }
+                else // Если не удалась, цикл обрывается
+                {
+                    check = 1;
+                    break;
+                }
+            }
+            if (check == 0) return Convert.ToString(sum); //конвертируюется в строку
+            else return "В списке есть не число";
+        }
+    }
+}
