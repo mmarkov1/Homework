46f917c46ad17bda1353620a7bd0b0ba997839c7
Максим Марков <legkiyprocces@mail.ru>
2021-11-16 21:00:18 +0500

    Добавлено 2 задание

---
+++ lesson5/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentTime = DateTime.Now.TimeOfDay.ToString();
+            string filename = "startup.txt";
+            File.AppendAllLines(filename, new[] { currentTime });  //Дописывает текущее время в новую строку в файле, а не переписывает сам файл
+            Console.ReadLine();
+        }
+    }
+}
