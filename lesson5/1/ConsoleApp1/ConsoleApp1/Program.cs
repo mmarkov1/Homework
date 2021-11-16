81c205c72cdde99d7eb6cb335c8b3184a7268928
Максим Марков <legkiyprocces@mail.ru>
2021-11-16 20:47:02 +0500

    Добавлено 1 задание

---
+++ lesson5/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string filename = "text_file.txt";
+            File.WriteAllText(filename, text);
+            Console.ReadLine();
+        }
+    }
+}
